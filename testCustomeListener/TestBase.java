4c2340124ef9677c3c76d945492ce585128b9da3
sayoojts <sayoojts87@gmail.com>
2018-07-29 21:36:22 +0530

    uploading via drag and drop

---
+++ testCustomeListener/TestBase.java
@@ -0,0 +1,14 @@
+package testCustomeListener;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestBase {
+	public static WebDriver driver;
+	
+	public void init(){
+		System.setProperty(arg0, arg1);
+		test
+	}
+	
+	
+}
